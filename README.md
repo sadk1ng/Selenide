53f4fec072e511800f58bd2a996119872078e8ae
Danila Leonidov <3317451@gmail.com>
2024-04-06 08:14:10 +0300

    Change

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ![Состояние сборки](https://ci.appveyor.com/api/projects/status/n2pu1hxubs7vf4ql/branch/mai?svg=true)
+https://ci.appveyor.com/project/sadk1ng/selenide
--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
